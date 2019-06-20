dba040e3e30bfd183cf73873d827bfcf3b54740a
Seth Schwartz <sschwartz214@gmail.com>
2019-06-20 17:02:59 -0500

    Completes unicorn

---
+++ exercises/unicorn.js
@@ -0,0 +1,16 @@
+class Unicorn {
+  constructor(name, color = "white") {
+    this.name = name;
+    this.color = color;
+  }
+
+  isWhite() {
+    return this.color === "white";
+  }
+
+  says(words) {
+    return `**;* ${words} *;**`;
+  }
+}
+
+module.exports = Unicorn;
